6e2aadb0eed0802ed7bac16841a714c21528d896
Bohdan <bodichka322@gmail.com>
2023-04-18 18:15:18 +0300

    second commit

--- src/components/TypesEntry.tsx
+++ src/components/TypesEntry.tsx
@@ -10,7 +10,7 @@ const TypesEntry = ({ brand }: any) => {
         <img className = 'types-entry__url ' src={brand.url} alt="Img" />
         <button> BUY </button>
         
-        {/* <img  src={'https://c.static-nike.com/a/images/w_1920,c_limit/mdbgldn6yg1gg88jomci/image.jpg'} style = {{height: "15px", width: "15px"}} alt="" /> */}
+        
         
 
     </div>
